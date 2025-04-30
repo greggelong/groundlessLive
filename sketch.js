debbd38c759d8675c48ce99bc40297a6cb7fc3bf
Greg K <gkreisman_cep@qq.com>
2025-04-30 22:50:54 +0800

    make quiteer

--- sketch.js
+++ sketch.js
@@ -46,7 +46,7 @@ function draw() {
   //vol = constrain(vol, 0.2, 0.9); // Prevent overshoot or dead zones
 
   // Map the volume to n between 1 and 5 (adjust these numbers to suit your needs)
-  n = map(vol, 0.2, 0.9, 1, 8); // Adjust max volume threshold as needed
+  n = map(vol, 0, 0.1, 1, 8); // Adjust max volume threshold as needed
   // change the vol mapping to 0 , 0.5 or 0, 1, to have a less sensitive mic
   //m = map(vol, 0,0.1,1,4)
   // Loop through the grid
